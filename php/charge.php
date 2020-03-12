2f1a5ab4f6354d39e59e7125e52fe884aa81fc58
bartosz-wojcik <bartosz.wojcik@mohawkcollege.ca>
2020-03-12 14:03:08 -0400

    trying to get form to work

---
+++ php/charge.php
@@ -0,0 +1,71 @@
+
+<?php
+
+require_once('vendor\autoload.php');
+//   require_once('config/db.php');
+//   require_once('lib/pdo_db.php');
+//   require_once('models/Customer.php');
+//   require_once('models/Transaction.php');
+
+\Stripe\Stripe::setApiKey('sk_test_BJJNoaDhqEnjBFP6vE78QXSa00H2xzeLHO');
+
+ // Sanitize POST Array
+ $POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
+
+ $first_name = $POST['first_name'];
+ $last_name = $POST['last_name'];
+ $email = $POST['email'];
+ $amount = $POST['amount'];
+ $token = $POST['stripeToken'];
+ 
+
+// Create Customer In Stripe
+$customer = \Stripe\Customer::create(array(
+  "email" => $email,
+  "source" => $token
+));
+
+// Charge Customer
+$charge = \Stripe\Charge::create(array(
+  "amount" => $amount*100,
+  "currency" => "usd",
+  "description" => "Testing for RMHCSCO",
+  "customer" => $customer->id
+));
+
+//Redirect to success
+header("Location: php/success.php?tid=".$charge->id. "&product=" .$charge->description);
+
+// // Customer Data
+// $customerData = [
+//   'id' => $charge->customer,
+//   'first_name' => $first_name,
+//   'last_name' => $last_name,
+//   'email' => $email
+// ];
+
+// // Instantiate Customer
+// $customer = new Customer();
+
+// // Add Customer To DB
+// $customer->addCustomer($customerData);
+
+
+// // Transaction Data
+// $transactionData = [
+//   'id' => $charge->id,
+//   'customer_id' => $charge->customer,
+//   'product' => $charge->description,
+//   'amount' => $charge->amount,
+//   'currency' => $charge->currency,
+//   'status' => $charge->status,
+// ];
+
+// // Instantiate Transaction
+// $transaction = new Transaction();
+
+// // Add Transaction To DB
+// $transaction->addTransaction($transactionData);
+
+// // Redirect to success
+// header('Location: php/success.php?tid='.$charge->id.'&product='.$charge->description);

9c0077a7dd1aae2605d0e476cc6c4a0c263b73dd
Sasha <sasha@Aleksandars-MacBook-Pro.local>
2020-03-13 12:36:21 -0400

    "Facebook Link Update"

--- js/cove8JS.js
+++ js/cove8JS.js
@@ -1,22 +1,22 @@
 // Loads when document when ready
 $(document).ready(function () {
 
-    console.log("PAGE LOADED")
-    // AJAX CALL - For Metrics Bars
-    $.ajax({
-        url: '../php/controller.php',
-        data: {
-            action: 'total_by_ward'
-        },
-        type: 'post',
-        success: function (output) {
-            alert(output);
-            console.log("success")
-        },
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-            alert("Status: " + textStatus);
-            alert("Error: " + errorThrown);
-        }
-    });
+    // console.log("PAGE LOADED")
+    // // AJAX CALL - For Metrics Bars
+    // $.ajax({
+    //     url: '../php/controller.php',
+    //     data: {
+    //         action: 'total_by_ward'
+    //     },
+    //     type: 'post',
+    //     success: function (output) {
+    //         alert(output);
+    //         console.log("success")
+    //     },
+    //     error: function (XMLHttpRequest, textStatus, errorThrown) {
+    //         alert("Status: " + textStatus);
+    //         alert("Error: " + errorThrown);
+    //     }
+    // });
 
 });

cce8764c54c0bb2baf92c1f8f584de7924e98094
Sasha <sasha@Aleksandars-MacBook-Pro.local>
2020-03-13 10:59:14 -0400

    Modal Update

--- php/model.php
+++ php/model.php
@@ -4,14 +4,22 @@
 $TPL['results'];
 $TPL['error'];
 $TPL['success'];
-//DID THIS WORK?
 
-function getData($conn)
+/**
+ * Function that inserts a new field into the phonebook Database
+ * Returns propgateTable to update view
+ */
+
+function getTotalByWard($conn)
 {
     try
     {
         // build the select query
-        $stmt = $conn->prepare("SELECT * FROM italkt5_rmhcsco");
+        $stmt = $conn->prepare("SELECT SUM(Amount) AS 'Total By Ward', w.Ward_ID, w.Ward_Name
+                                FROM Donation d
+                                JOIN Ward w ON d.Ward_ID = w.Ward_ID
+                                GROUP BY Ward_ID
+                                ORDER BY SUM(AMOUNT) DESC ");
 
         // execute the query, save reference to results
         $stmt->execute();
@@ -36,11 +44,42 @@ function getData($conn)
  * Function that inserts a new field into the phonebook Database
  * Returns propgateTable to update view
  */
-function insert($conn, $first_name, $last_name, $email, $phone)
+
+function getTotal($conn)
+{
+    try
+    {
+        // build the select query
+        $stmt = $conn->prepare("SELECT SUM(Amount) AS 'Total' FROM Donation");
+
+        // execute the query, save reference to results
+        $stmt->execute();
+
+        // grab results of query for as long as results, store in TPL
+        while ($nextRow = $stmt->fetch()) {
+            $TPL['results'][] = $nextRow;
+        }
+        // print_r($TPL['results']);
+
+    } catch (PDOException $e) {
+        // echo "Select failed: " . $e->getMessage();
+        $TPL['error'] = "Select failed: " . $e->getMessage();
+        exit();
+    }
+
+    return $TPL['results'];
+
+}
+
+/**
+ * Function that inserts a new field into the phonebook Database
+ * Returns propgateTable to update view
+ */
+function insert($conn, $amount, $date_time, $ward_id)
 {
     try {
-        $stmt = $conn->prepare("INSERT INTO phonebook (fname, lname, phone, email) VALUES (?, ?, ?, ?)");
-        $stmt->execute([$first_name, $last_name, $email, $phone]);
+        $stmt = $conn->prepare("INSERT INTO Donation (Amount, Date/Time, Ward_ID) VALUES (?,?,?);");
+        $stmt->execute([$amount, $date_time, $email, $ward_id]);
         return $success = "One Record Was Successfully Inserted";
 
     } catch (PDOException $e) {

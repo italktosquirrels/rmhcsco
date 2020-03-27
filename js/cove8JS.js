628093548bc650538c0decaaf47d54f74d82545a
Sasha <Sasha@Sasha-iMac.local>
2020-03-26 21:54:32 -0400

    "Maps Datalayer Setup

--- js/cove8JS.js
+++ js/cove8JS.js
@@ -12,11 +12,27 @@ $(document).ready(function () {
         var rank = 1;
         var styles_li = {
             height: "63.63px",
+            // "text-transform": "uppercase"
         };
 
         var styles_span1 = {
-            "font-size": "1.5em",
-            padding: "5px",
+            "font-family": 'Futura Bold',
+            "font-size": "1vw",
+            padding: "0px 15px",
+            // "padding-left": "15px"
+        };
+
+        var styles_span2 = {
+            "font-size": "1vw",
+            "font-family": 'Futura',
+            // padding: "5px",
+        };
+
+        var styles_span3 = {
+            "font-family": 'Futura Bold',
+            "font-size": "1vw",
+            "text-align": "right",
+            padding: "15px 0px 0px 15px",
         };
 
         $.ajax({
@@ -35,8 +51,9 @@ $(document).ready(function () {
                 $("#total_donations").text(data.totalDonations[0].Total);
 
                 $.each(data.totalByWard, function (key, value) {
-                    $(".sidebar").append('<li style="border-bottom: 3px solid ' + data.totalByWard[key].Ward_Colour + ';border-left: 10px solid ' + data.totalByWard[key].Ward_Colour + ';"><span class="span1">' + rank + '</span>' + data.totalByWard[key].Ward_Name + ' ' + data.totalByWard[key].Amount + '</li>');
-                    console.log(rank, data.totalByWard[key].Ward_Name, data.totalByWard[key].Amount, data.totalByWard[key].Ward_Colour);
+                    $(".sidebar").append('<li style="border-bottom: 1.5px solid ' + data.totalByWard[key].Ward_Colour + ';border-left: 5px solid ' +
+                        data.totalByWard[key].Ward_Colour + ';"><span class="span1">' + rank + '</span><span class="span2">' + data.totalByWard[key].Ward_Name + '</span><span class="span3">$' + data.totalByWard[key].Amount + '.00</span></li > ');
+                    // console.log(rank, data.totalByWard[key].Ward_Name, data.totalByWard[key].Amount, data.totalByWard[key].Ward_Colour);
                     console.log('<li style="border-bottom: 3px solid ' + data.totalByWard[key].Ward_Colour + '; border-left: 10px solid ' + data.totalByWard[key].Ward_Colour + ';"><span id="span1">' + rank + '</span>' + data.totalByWard[key].Ward_Name + ' ' + data.totalByWard[key].Amount + '</li>');
 
                     rank++;
@@ -44,6 +61,8 @@ $(document).ready(function () {
 
                 $(".sidebar li").css(styles_li);
                 $(".span1").css(styles_span1);
+                $(".span2").css(styles_span2);
+                $(".span3").css(styles_span3);
 
 
             },
--- js/map.js
+++ js/map.js
@@ -5,20 +5,48 @@
           // create the map
           map = new google.maps.Map(document.getElementById('map'), {
               center: new google.maps.LatLng(43.2609686, -80.2133423),
-              zoom: 10,
+              zoom: 11,
               mapTypeId: 'terrain'
           });
 
-          // load the kml data from the source 
-          var kmlLayer = new google.maps.KmlLayer(src, {
-              suppressInfoWindows: true,
-              preserveViewport: false,
-              map: map
+          var geoJsonData = map.data.loadGeoJson('data/Ward_Boundaries.json');
+
+          console.log(geoJsonData);
+
+          fetch('data/Ward_Boundaries.json')
+              .then(response => response.text())
+              .then((data) => {
+                  console.log(data)
+                  map.data.addGeoJson(data);
+              })
+
+          map.data.setStyle(function (feature) {
+              var SD_NAME = feature.getProperty('WARD');
+              var color = "gray";
+              if (SD_NAME == "1") {
+                  color = "red";
+              }
+              return {
+                  fillColor: color,
+                  strokeWeight: 1
+              }
           });
-          // add listener to show ward information onclick
-          kmlLayer.addListener('click', function (event) {
-              var content = event.featureData.infoWindowHtml;
-              var testimonial = document.getElementById('sidebar');
-              testimonial.innerHTML = content;
-          });
+
+          console.log('mao');
+
       }
+
+
+
+      //   // load the kml data from the source 
+      //   var kmlLayer = new google.maps.KmlLayer(src, {
+      //       suppressInfoWindows: true,
+      //       preserveViewport: false,
+      //       map: map
+      //   });
+      //   // add listener to show ward information onclick
+      //   kmlLayer.addListener('click', function (event) {
+      //       var content = event.featureData.infoWindowHtml;
+      //       var testimonial = document.getElementById('sidebar');
+      //       testimonial.innerHTML = content;
+      //   });

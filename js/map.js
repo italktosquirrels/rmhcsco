b91923612fe7ab7d6b49e1d2292b8186d6eb0bc8
elizabeth <elizabethsouthward1@gmail.com>
2020-03-30 17:30:45 -0400

    map

--- js/map.js
+++ js/map.js
@@ -148,7 +148,7 @@ function initMap() {
     });
 
     //RMHSCO LOCATION AND MARKER
-    var image = 'img/rmhcsco_map_icon.png';
+    var image = 'RMHCSCO/img/rmhcsco_map_icon.png';
     var beachMarker = new google.maps.Marker({
         position: {
             lat: 43.258030,
@@ -164,7 +164,7 @@ function initMap() {
 
     map.data.setStyle(function (feature) {
         var WARD = feature.getProperty('WARD');
-        var color = "gray";
+        var color = "gray"; // for testing. prev : 'gray'
         switch (WARD) {
 
             case "1":
@@ -221,6 +221,6 @@ function initMap() {
         }
     });
 
-    console.log('mao');
+    console.log('testtesttest');
 
 }
